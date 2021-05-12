16ef00c2d08f930a7984fc14fefe0b9e4a51818a
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-05-12 15:51:15 -0300

    fix: Changing jquery to execute click function in any link inside types-filter class

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,67 +1,67 @@
 // JS for the buttons on the home page
-$(document).ready(function(){
-  $(".types-filter-button").click(function() {
-    $(this).parent().find('.types-filter').toggle();
-  });
+$(document).ready(function() {
+    $(".types-filter-button").click(function() {
+        $(this).parent().find('.types-filter').toggle();
+    });
 
-  $(".types-filter > li a").click(function(e){
-    e.preventDefault();
+    $(".types-filter").find("li a").click(function(e) {
+        e.preventDefault();
 
-    $(this).parent().parent().parent().find(".types-filter").hide();
-    var language = this.getAttribute("data-language");
-    $(this).parent().parent().parent().find(".types-filter-button").html(language);
-    var queryKey = this.getAttribute("data-query");
-    updateQueryStringParam(queryKey, language);
-    removeQueryStringParam("page");
-    document.location = document.URL;
-//     $.ajax({
-//       url: document.URL,
-//       headers: {
-//         Accept :        "application/json; charset=utf-8",
-//         "Content-Type": "application/json; charset=utf-8"
-//       },
-//       beforeSend: function () {
-//         // go
-//       },
-//     }).done(function(data) {
-//       $('.repo-list-with-pagination').html(data["html"]);
-//     }).fail(function(data){
-//       console.log(data);
-//     });
+        $(this).parent().parent().parent().find(".types-filter").hide();
+        var language = this.getAttribute("data-language");
+        $(this).parent().parent().parent().find(".types-filter-button").html(language);
+        var queryKey = this.getAttribute("data-query");
+        updateQueryStringParam(queryKey, language);
+        removeQueryStringParam("page");
+        document.location = document.URL;
+        //     $.ajax({
+        //       url: document.URL,
+        //       headers: {
+        //         Accept :        "application/json; charset=utf-8",
+        //         "Content-Type": "application/json; charset=utf-8"
+        //       },
+        //       beforeSend: function () {
+        //         // go
+        //       },
+        //     }).done(function(data) {
+        //       $('.repo-list-with-pagination').html(data["html"]);
+        //     }).fail(function(data){
+        //       console.log(data);
+        //     });
 
-    return false;
-  });
+        return false;
+    });
 })
 
 function baseUrl() {
-  return [location.protocol, '//', location.host, location.pathname].join('');
+    return [location.protocol, '//', location.host, location.pathname].join('');
 }
 
 function removeQueryStringParam(param) {
-  var urlQueryString = document.location.search;
+    var urlQueryString = document.location.search;
 
-  if (urlQueryString) {
-    keyRegex = new RegExp('&?' + param + '[^&]*');
-    params = urlQueryString.replace(keyRegex, '');
-  }
+    if (urlQueryString) {
+        keyRegex = new RegExp('&?' + param + '[^&]*');
+        params = urlQueryString.replace(keyRegex, '');
+    }
 
-  window.history.replaceState({}, '', baseUrl() + params);
+    window.history.replaceState({}, '', baseUrl() + params);
 }
 
 function updateQueryStringParam(param, value) {
-  urlQueryString = document.location.search;
-  var newParam = param + '=' + encodeURIComponent(value),
-  params = '?' + newParam;
+    urlQueryString = document.location.search;
+    var newParam = param + '=' + encodeURIComponent(value),
+        params = '?' + newParam;
 
-  // If the "search" string exists, then build params from it
-  if (urlQueryString) {
-    keyRegex = new RegExp('([\?&])' + param + '[^&]*');
-    // If param exists already, update it
-    if (urlQueryString.match(keyRegex) !== null) {
-      params = urlQueryString.replace(keyRegex, "$1" + newParam);
-    } else { // Otherwise, add it to end of query string
-      params = urlQueryString + '&' + newParam;
+    // If the "search" string exists, then build params from it
+    if (urlQueryString) {
+        keyRegex = new RegExp('([\?&])' + param + '[^&]*');
+        // If param exists already, update it
+        if (urlQueryString.match(keyRegex) !== null) {
+            params = urlQueryString.replace(keyRegex, "$1" + newParam);
+        } else { // Otherwise, add it to end of query string
+            params = urlQueryString + '&' + newParam;
+        }
     }
-  }
-  window.history.replaceState({}, "", baseUrl() + params);
+    window.history.replaceState({}, "", baseUrl() + params);
 };
