c2e2c8636bd929d8e238fbf3aa2b164773bb27f4
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-05-13 17:49:46 -0300

    Revert "Merge pull request #1 from MarcosCastelo/docker"
    This reverts commit ddb5fb876400f8a9a115e208799e056ce14a346b, reversing
    changes made to 1f4cf900432f471dfaf2ee3cfaea3f8727d09e59.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,17 +2,11 @@
 
 unless Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = {
-      host: 'redis',
-      port: '6379'
-    }
+    config.redis = { url: ENV["REDIS_URL"], namespace: "codetriage-sidekiq" }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = {
-      host: 'redis',
-      port: '6379'
-    }
+    config.redis = { url: ENV["REDIS_URL"], namespace: "codetriage-sidekiq" }
   end
 end
 
